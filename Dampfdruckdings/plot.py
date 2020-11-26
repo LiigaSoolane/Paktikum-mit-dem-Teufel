9eb21f6fb9de478bf844a7910f88fe2b1e4b2a5a
LiigaSoolane <mirjam.prayer@gmx.de>
2020-11-26 22:06:22 +0100

    Anfang

--- Dampfdruckdings/plot.py
+++ Dampfdruckdings/plot.py
@@ -1,21 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+p,t=np.genfromtxt("messwerte.txt", unpack=True)
+t=t+273.15
+p=p*10**(2)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+x_plot=1/t
+y_plot=np.log(p)
+xfit = np.linspace(x_plot[0],x_plot[49],80)
+params, cov = np.polyfit(x_plot, y_plot, deg=1, cov=True)
+print('a,b = ',params)
+errors2 = np.sqrt(np.diag(cov))
+print('aerr,berr=',errors2)
+L = ufloat(-params[0]*8.31446261815324,errors2[0]*8.31446261815324)
+error=errors2[0]*8.31446261815324
+print('Lerr=',error)
+print('L = ', L)
+
+plt.figure(0)
+plt.plot(x_plot, y_plot, 'r*', label='Messdaten')
+plt.plot(xfit, params[0]*xfit+params[1], 'g-', label='fit')
+plt.ylabel(r'ln($\dfrac{p}{p_0}$)')
+plt.xlabel(r'1/Temperatur [1/K]')
 plt.legend(loc='best')
+plt.savefig('plot.pdf')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+
