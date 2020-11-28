371a49aa9ceb29b46cbf7e34e3ab140922242490
LiigaSoolane <mirjam.prayer@gmx.de>
2020-11-28 14:54:44 +0100

    d)

--- Dampfdruckdings/content/plot.py
+++ Dampfdruckdings/content/plot.py
@@ -6,19 +6,21 @@ from uncertainties import ufloat
 p,t=np.genfromtxt("messwerte.txt", unpack=True)
 t=t+273.15
 p=p*10**(2)
+p0=100300
 r = 8.31446261815324
 
 np.savetxt("aufgabe4.txt", np.column_stack([p, t]), fmt = "%10.4f", delimiter = " & ", newline = " \\\ ", header = " Tka Tsa ")
 
 x_plot=1/t
-y_plot=np.log(p)
+y_plot=np.log(p/p0)
 xfit = np.linspace(x_plot[0],x_plot[49],80)
 params, cov = np.polyfit(x_plot, y_plot, deg=1, cov=True)
 print('a,b = ',params)
 errors2 = np.sqrt(np.diag(cov))
 print('aerr,berr=',errors2)
+print('L = ', -params[0]*8.31446261815324)
+print('Lerror', errors2[0]*8.31446261815324)
 L = ufloat(-params[0]*8.31446261815324,errors2[0]*8.31446261815324)
-print('L = ', L)
 
 plt.figure(0)
 plt.plot(x_plot, y_plot, 'r*', label='Messdaten')
@@ -28,6 +30,8 @@ plt.xlabel(r'1/Temperatur [1/K]')
 plt.legend(loc='best')
 plt.savefig('build/plot.pdf')
 
+La = r*273
+Li = L-La
 
 p,t=np.genfromtxt("messwerte.txt", unpack=True)
 t=t+273.15
@@ -63,8 +67,8 @@ plt.savefig('build/plot1.pdf')
 #plt.legend(loc='best')
 #plt.savefig('build/plot2.pdf')
 #
-#lt = dpt/pt*(r*t/2-((r*t/2)**2-S*pt)**(1/2))
-#plt.figure(3)
+#lt = dpt*pt*(r*t/2-((r*t/(2*pt)**2-S/pt)**(1/2))
+#plt.figure(2)
 #plt.plot(t, lt, 'r*', label='L')
 #plt.ylabel(r'L(T)')
 #plt.xlabel(r'Temperatur [K]')
