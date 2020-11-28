d9035b30001d6e2a9faee3bc37a8c15815320543
LiigaSoolane <mirjam.prayer@gmx.de>
2020-11-28 19:54:24 +0100

    Auswertung fertig

--- Dampfdruckdings/plot.py
+++ Dampfdruckdings/plot.py
@@ -9,8 +9,6 @@ p=p*10**(2)
 p0=100300
 r = 8.31446261815324
 
-np.savetxt("aufgabe4.txt", np.column_stack([p, t]), fmt = "%10.4f", delimiter = " & ", newline = " \\\ ", header = " Tka Tsa ")
-
 x_plot=1/t
 y_plot=np.log(p/p0)
 xfit = np.linspace(x_plot[0],x_plot[49],80)
@@ -32,10 +30,12 @@ plt.savefig('build/plot.pdf')
 
 La = r*273
 Li = L-La
+print(La, Li)
 
-p,t=np.genfromtxt("messwerte.txt", unpack=True)
+p,t=np.genfromtxt("messwerte1.txt", unpack=True)
 t=t+273.15
 p=p*10**(5)
+np.savetxt("aufgabe3.txt", np.column_stack([p, t]), fmt = "%10.4f", delimiter = " & ", newline = " \\\ ", header = " Tka Tsa ")
 S=0.9
 params, cov = np.polyfit(t, p, deg=3, cov=True)
 print('a,b = ',params)
@@ -50,7 +50,7 @@ upt = a*t**3+b*t**2+c*t+d
 dpt= 3*params[0]*t**2+2*params[1]*t+params[2]
 udpt= 3*a*t**2+2*b*t+c
 
-plt.figure(1)
+plt.figure("""first figure""")
 plt.plot(t, p, 'r*', label='Messdaten')
 plt.plot(t, pt, 'g-', label='fit')
 plt.ylabel(r'$p$ [Pa]')
@@ -58,20 +58,18 @@ plt.xlabel(r'Temperatur [K]')
 plt.legend(loc='best')
 plt.savefig('build/plot1.pdf')
 
-#print(lt[2])
-#lt = dpt/pt*(r*t/2+(-(r*t/2)**2+S*pt)**(1/2))
-#plt.figure(2)
-#plt.plot(t, lt, 'r*', label='L')
-#plt.ylabel(r'L(T)')
-#plt.xlabel(r'Temperatur [K]')
-#plt.legend(loc='best')
-#plt.savefig('build/plot2.pdf')
-#
-#lt = dpt*pt*(r*t/2-((r*t/(2*pt)**2-S/pt)**(1/2))
-#plt.figure(2)
-#plt.plot(t, lt, 'r*', label='L')
-#plt.ylabel(r'L(T)')
-#plt.xlabel(r'Temperatur [K]')
-#plt.legend(loc='best')
-#plt.savefig('build/plot3.pdf')
-#
+lt = dpt*t*(r*t/(2*pt)+((r*t/(2*pt))**2-S/pt)**(1/2))
+plt.figure("""second figure""")
+plt.plot(t, lt, 'r*', label='L zu den Messwerten')
+plt.ylabel(r'L(T) in J/mol')
+plt.xlabel(r'Temperatur [K]')
+plt.legend(loc='best')
+plt.savefig('build/plot2.pdf')
+
+lt = dpt*t*(r*t/(2*pt)-((r*t/(2*pt))**2-S/pt)**(1/2))
+plt.figure("""forth Figure""")
+plt.plot(t, lt, 'r*', label='L zu den Messwerten')
+plt.ylabel(r'L(T) in $J/mol')
+plt.xlabel(r'Temperatur [K]')
+plt.legend(loc='best')
+plt.savefig('build/plot3.pdf')
