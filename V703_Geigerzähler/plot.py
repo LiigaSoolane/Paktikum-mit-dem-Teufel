9b101f15b687423840a5ff45fd2d105ddaef2577
Nikola Mang <nikola.mang@tu-dortmund.de>
2021-01-17 22:50:16 +0100

    wir sollten bei einem ordner pro Versuch bleiben

--- V703_Geigerzähler/plot.py
+++ V703_Geigerzähler/plot.py
@@ -1,21 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+Un,N=np.genfromtxt("Kennlinie.dat", unpack=True)
+Ui, I=np.genfromtxt("Zaehlrohrstrom.dat", unpack=True)
+I=I*10**(-6)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+# a)
+params, cov = np.polyfit(Un[5:34], N[5:34], deg=1, cov=True) #370-640 V
+errors = np.sqrt(np.diag(cov))
+for name, value, error in zip('ab', params, errors):
+    print(f'{name} = {value:.3f} ± {error:.3f}')
+fit = params[0]*Un[5:34]+params[1]
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+plt.errorbar(Un, N, yerr=N**(1/2), fmt='ro', label='Messwerte')
+plt.plot(Un[5:34], fit, label='lineare Regression')
+plt.xlabel(r'U [V]')
+plt.ylabel(r'N [Imp]')
+plt.savefig('plot.pdf')
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+# b) entfällt
+
+# c)
+#Totzeit anhand der Zwei-Quellen-Methode
+N1 = 96401
+N2 = 76518
+N12 = 158479
+T=(N1+N2-N12)/(2*N1*N2) 
+
+#Totzeit anhand des Oszilloskops
+
+# d)
+e0 = 1.602176634*10**(-19)
+Z = np.zeros(8)
+for i in range(7):
+    Z[i] = I[i]/(e0*N[3+5*i])
+print(f'Z = ', Z)
+
+
