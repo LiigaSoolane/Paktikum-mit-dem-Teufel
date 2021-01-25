fbee478976d248be82ea0782d5793356202603b3
Nikola Mang <nikola.mang@tu-dortmund.de>
2021-01-25 19:32:24 +0100

    it is late my firends

--- V702_Neutronenaktivierung/plot.py
+++ V702_Neutronenaktivierung/plot.py
@@ -1,21 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+tv, Nv = np.genfromtxt('Vanadium.dat', unpack=True)
+tr, Nr = np.genfromtxt('Rhodium.dat', unpack=True)
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
-plt.legend(loc='best')
+Nu = np.array(129, 143, 144, 136, 126, 158)
+tu = 300
 
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
