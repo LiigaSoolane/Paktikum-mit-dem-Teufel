ea97d8d79be4820a7c599d0189673403b6595df0
Nikola Mang <nikola.mang@tu-dortmund.de>
2021-01-28 01:53:28 +0100

    fuck everything

--- V702_Neutronenaktivierung/rhodium.py
+++ V702_Neutronenaktivierung/rhodium.py
@@ -19,17 +19,17 @@ def lnn(x, lamb, const):
     return - lamb * x + const
 
 # curvefitear la parte mas lenta
-paramsl, covl = curve_fit(lnn, tr[28:], unp.nominal_values(Nr_diff_log[28:]), sigma=unp.std_devs(Nr_diff_log[28:]))
+paramsl, covl = curve_fit(lnn, tr[27:], unp.nominal_values(Nr_diff_log[27:]), sigma=unp.std_devs(Nr_diff_log[27:]))
 errorsl = np.sqrt(np.diag(covl))
 lambl = unp.uarray(paramsl[0], errorsl[0])
 constl = unp.uarray(paramsl[1], errorsl[1])
 
 # numero de impulsos de la parte mas rapida
 Nr_k = Nr_diff - unp.exp(lnn(tr, lambl, constl))
-Nr_k_log = unp.log(Nr_k[:14])
+Nr_k_log = unp.log(Nr_k[:8])
 
 # curvefitear la parte mas rapida
-paramsk, covk = curve_fit(lnn, tr[:14], unp.nominal_values(Nr_k_log), sigma=unp.std_devs(Nr_k_log))
+paramsk, covk = curve_fit(lnn, tr[:8], unp.nominal_values(Nr_k_log), sigma=unp.std_devs(Nr_k_log))
 errorsk = np.sqrt(np.diag(covk))
 lambk = unp.uarray(paramsk[0], errorsk[0])
 constk = unp.uarray(paramsk[1], errorsk[1])
@@ -44,20 +44,19 @@ print(f'const slow = {constl}, const fast = {constk}')
 print(f'Halbwertszeit slow = {Tl}, Halbwertszeit fast = {Tk}')
 
 # hacer el grafico
-tstern = 420
-tmax = 200
+tstern = 400
+tmax = 120
 x = np.linspace(0, 630)
 xl = np.linspace(tstern, 630)
 xk = np.linspace(0, tmax)
 
-#plt.errorbar(tr, unp.nominal_values(unp.log(Nr_diff_log)), yerr = unp.std_devs(unp.log(Nr_diff_log)), fmt='b.', label="Messwerte mit Fehlern")
-#plt.plot(xl, unp.nominal_values(lnn(xl, *paramsl)), 'r--', label='Fit für langsamen Zerfall')
-#plt.plot(xk, unp.nominal_values(lnn(xk, *paramsk)), 'y--', label='Fit für schnellen Zerfall')
-#plt.plot(x, unp.nominal_values(lnn(x, *paramsl) + lnn(x, *paramsk)), 'g-', label='ich will schlafen')
-#plt.yscale('log')
-#plt.xlabel('t [s]')
-#plt.ylabel('ln(N)')
-#plt.legend()
-#plt.tight_layout()
-##plt.safefig('rhodium.pdf')
-#plt.show()
+plt.errorbar(tr, unp.nominal_values(Nr_diff_log), yerr = unp.std_devs(Nr_diff_log), fmt='b.', label="Messwerte mit Fehlern")
+plt.plot(xl, unp.nominal_values(lnn(xl, *paramsl)), 'r--', label='Fit für langsamen Zerfall')
+plt.plot(xk, unp.nominal_values(lnn(xk, *paramsk)), 'y--', label='Fit für schnellen Zerfall')
+#plt.plot(tr, unp.nominal_values(lnn(tr, *paramsl)) + unp.nominal_values(lnn(tr, *paramsk)), 'g-', label='ich will schlafen')
+plt.yscale('log')
+plt.xlabel('t [s]')
+plt.ylabel('ln(N)')
+plt.legend()
+plt.tight_layout()
+plt.savefig('rhodium.pdf')
