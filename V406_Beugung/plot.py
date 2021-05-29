b8160b917982d60d94f76a38ad4a39916cce0950
LiigaSoolane <mirjam.prayer@gmx.de>
2021-05-29 21:08:16 +0200

    Der Plan von der Abschaffung des Dunkels

--- V406_Beugung/plot.py
+++ V406_Beugung/plot.py
@@ -1,21 +1,105 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties.unumpy as unp
+from uncertainties.unumpy import (nominal_values as noms,
+                                  std_devs as stds)
+from scipy import stats
+from scipy.optimize import curve_fit
+import scipy.constants as const
+from tabulate import tabulate
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+###############################################################################
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+#funktsioonid
+
+def einzel(phi,b, a):
+    lamda=633*10**(-9)
+    uff=(a**2)*(b**2)*((lamda/(np.pi*b*np.sin(phi)))**2)
+    teil2=(np.sin((np.pi*b*np.sin(phi))/lamda))**2
+    return(uff*teil2)
+
+def doppel(phi,b,A_0,s):
+    lamda=633*10**(-9)
+    teil1=(A_0**2)*np.cos((np.pi*s*np.sin(phi)/lamda))**2
+    teil2=(lamda/(np.pi*b*np.sin(phi)))**2
+    teil3=np.sin((np.pi*b*np.sin(phi))/lamda)**2
+    return(teil1*teil2*teil3)
+
+
+def a(ag,at):
+    return(np.absolute(ag-at)/(at))
+
+#######################################################################################
+
+#Einzelspalt
+
+#reading data
+L = 1.047
+I_d = 2.27*10**(-9)
+
+d, I= np.genfromtxt('data.txt',unpack=True)
+d = (d*10**(-3))-0.026
+I = (I*10**(-3))-I_d
+phi=(d)/L
+
+
+
+#走吧
+table ={'1': d, '3': I}
+print(tabulate (table, tablefmt="latex"))
+I1=I[1:50]
+phi1=phi[1:50]
+
+params, covariance =curve_fit(einzel,phi1,I1,p0=[0.00015,70])
+errors = np.sqrt(np.diag(covariance))
+print('breite des Spaltes:', params[0],'+-',errors[0])
+print('Abweichung', a(params[0],0.000075))
+print('A_0:', params[1],'+-',errors[1])
+#def einzel(phi,b,lamda,A_0)
+
+x=np.linspace(-0.025,0.025,10000)
+
+plt.figure(1)
+plt.plot(phi,I,'rx',label=r'$Messwerte$')
+plt.plot(x,einzel(x,*params),'b-',label=r'$Ausgleichsfunktion$')
 plt.legend(loc='best')
+plt.xlabel(r'$\mathrm{Abstand \ von \ 0.-Hauptmaxima \ d \ in \ m}$')
+plt.ylabel(r'$\mathrm{Intensität \ I/A}$')
+plt.savefig('plot.pdf')
 
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \:/\: \si{\ohm}$')
-plt.ylabel(r'$y \:/\: \si{\micro\joule}$')
+
+###############################################################################
+
+#Doppelspalt
+
+#reading data
+d, I= np.genfromtxt('ddata.txt',unpack=True)
+d = (d*10**(-3))-0.026
+I = (I*10**(-3))-I_d
+phi=d/L
+
+#走吧
+table ={'1': d, '3': I}
+print(tabulate (table, tablefmt="latex"))
+I1=I[1:50]
+phi1=phi[1:50]
+print(phi1)
+print(I1)
+
+params, covariance =curve_fit(doppel,phi1,I1,p0=[0.00015,0.05, 1])
+errors = np.sqrt(np.diag(covariance))
+print('breite des Spaltes:', params[0],'+-',errors[0])
+print('Abweichung', a(params[0],0.000075))
+print('A_0:', params[1],'+-',errors[1])
+print('s:', params[2], '+-', errors[2])
+#def einzel(phi,b,lamda,A_0)
+
+x=np.linspace(-0.025,0.025,100)
+
+plt.figure(2)
+plt.plot(phi,I,'rx',label=r'$Messwerte$')
+plt.plot(x,doppel(x,*params),'b-',label=r'$Ausgleichsfunktion$')
 plt.legend(loc='best')
-
-# in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+plt.xlabel(r'$\mathrm{Abstand \ von \ 0.-Hauptmaxima \ d \ in \ m}$')
+plt.ylabel(r'$\mathrm{Intensität \ I/A}$')
+plt.savefig('plot1.pdf')
