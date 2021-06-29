21d3fc59bdce5a573e8ea66a56b551f57ebb62db
LiigaSoolane <mirjam.prayer@gmx.de>
2021-06-29 18:03:11 +0200

    Diese Droge pusht Sucht auf ein neues Level: Schreiben eines Praktikumberichts kann abhängig machen.

--- V500_Photoeffekt/plot.py
+++ V500_Photoeffekt/plot.py
@@ -36,24 +36,30 @@ c, yelug =pl(yelU, yelI, 'build/verdammtescheiße.pdf', c, 18, 28)
 c, yellug =pl(yellU, yellI, 'build/fresse.pdf', c, 8, 11)
 
 lamda = np.array([614, 577.579, 577.579, 546, 434])
+lamda = 299792458*10**9/(lamda)
 ug = np.array([redug, yelug, yellug, greenug, blueug])
 #table ={'1': lamda, '3': ug}
 #print(tabulate (table, tablefmt="latex"))
 params, cov = np.polyfit(lamda, ug, deg=1, cov=True)
 errors = np.sqrt(np.diag(cov))
-for name, value, error in zip('ab', params, errors):
-    print(f'{name} = {value:.3f} ± {error:.3f}')
-e= - 1.602176634 * 10**(-19)
-print(f'h=', params[0]*e)
 
-x0=np.linspace(614, 434, 100)
+for name, value, error in zip('ab', params, errors):
+    print(f'{name} = {value} ± {error}')
+
+
+x0=np.linspace(lamda[0], lamda[4], 100)
 y0=params[1]+x0*params[0]
+a=params[0]
 
 c+1
 plt.figure(c)
 plt.plot(lamda, ug, 'r*', label='Messwerte')
 plt.plot(x0, y0, label='lineare Regression')
-plt.xlabel(r'$ \lambda$ [nm]')
+plt.xlabel('f [Hz]')
 plt.ylabel(r'$U_G$ [V]')
 plt.legend(loc='best')
 plt.savefig('build/torte.pdf')
+
+theo=4.136*10**(-15)
+print((theo-a)/theo)
+
